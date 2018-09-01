154258038a8ef0692a14c8ef2ea31cc06faa6fce
Bryan W. Weber <bryan.w.weber@gmail.com>
2018-09-01 10:08:01 -0400

    Fix copy_handouts to actually copy the files
    The copy_tree function from Nikola only works if you supply a folder of 
    files to copy, not individual files.

--- plugins/copy_handouts.py
+++ plugins/copy_handouts.py
@@ -3,25 +3,15 @@ from pathlib import Path
 from nikola.plugin_categories import Task
 from nikola import utils
 
-import yaml
-
-dropbox = Path.home()/'Dropbox/Classes/2018-Fall-ME-2233'
-
 
 class CopyHandouts(Task):
 
     name = 'copy_handouts'
 
-    def set_site(self, site):
-        self.site = site
-
-        with open('class_config.yaml', 'r') as conf_file:
-            self.site.class_config = yaml.load(conf_file)
-        return super().set_site(site)
-
     def gen_tasks(self):
         kw = {
-            'quizzes': self.site.class_config['quizzes'],
+            'root': Path(self.site.GLOBAL_CONTEXT['data']['class_config']['root']).expanduser(),
+            'quizzes': self.site.GLOBAL_CONTEXT['data']['class_config']['quizzes'],
             'output_folder': Path(self.site.config['OUTPUT_FOLDER']),
             'filters': self.site.config['FILTERS'],
         }
@@ -31,8 +21,13 @@ class CopyHandouts(Task):
         dest = kw['output_folder']/'slides'
         filters = kw['filters']
         for handout in handouts:
-            handout_file = dropbox/f'topic-slides/{handout}.zip'
-            for task in utils.copy_tree(str(handout_file.resolve()), str(dest.resolve())):
-                task['basename'] = self.name
-                task['uptodate'] = [utils.config_changed(kw, self.name)]
-                yield utils.apply_filters(task, filters)
+            handout_file = kw['root']/f'topic-slides/{handout}.zip'
+            real_dest = dest/handout_file.name
+            yield utils.apply_filters({
+                'basename': self.name,
+                'name': str(real_dest),
+                'file_dep': [handout_file],
+                'targets': [real_dest],
+                'actions': [(utils.copy_file, (handout_file, real_dest))],
+                'uptodate': [utils.config_changed(kw, self.name)],
+            }, filters)
