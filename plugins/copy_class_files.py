bfde2433209b5d03c1d324bd9359d517770da59c
Bryan W. Weber <bryan.w.weber@gmail.com>
2018-09-01 20:31:01 -0400

    Move all file copying from Dropbox to site into one task class

--- plugins/copy_class_files.py
+++ plugins/copy_class_files.py
@@ -5,20 +5,47 @@ from nikola.plugin_categories import Task
 from nikola import utils
 
 
-class CopyHomework(Task):
+class CopyClassFiles(Task):
 
-    name = 'copy_homework'
+    name = 'copy_class_files'
 
     def gen_tasks(self):
         kw = {
             'root': Path(self.site.GLOBAL_CONTEXT['data']['class_config']['root']).expanduser(),
             'homework': self.site.GLOBAL_CONTEXT['data']['class_config']['homework'],
+            'quizzes': self.site.GLOBAL_CONTEXT['data']['class_config']['quizzes'],
+            'course-material': self.site.GLOBAL_CONTEXT['data']['class_config']['course-materials'],
             'output_folder': Path(self.site.config['OUTPUT_FOLDER']),
             'filters': self.site.config['FILTERS'],
         }
         yield self.group_task()
 
+        handouts = [q['due-date'] for q in kw['quizzes'] if q.get('handout', False)]
+        dest = kw['output_folder']/'slides'
         filters = kw['filters']
+        for handout in handouts:
+            handout_file = kw['root']/f'topic-slides/{handout}.zip'
+            real_dest = dest/handout_file.name
+            yield utils.apply_filters({
+                'basename': self.name,
+                'name': str(real_dest),
+                'file_dep': [handout_file],
+                'targets': [real_dest],
+                'actions': [(utils.copy_file, (handout_file, real_dest))],
+                'uptodate': [utils.config_changed(kw, self.name)],
+            }, filters)
+
+        syllabus_file = kw['root']/'syllabus/syllabus.pdf'
+        real_dest = kw['output_folder']/'syllabus/syllabus.pdf'
+        yield utils.apply_filters({
+            'basename': self.name,
+            'name': str(real_dest),
+            'file_dep': [syllabus_file],
+            'targets': [real_dest],
+            'actions': [(utils.copy_file, (syllabus_file, real_dest))],
+            'uptodate': [utils.config_changed(kw, self.name)],
+        }, filters)
+
         for homework in kw['homework']:
             hw_num = homework['number']
             src = kw['root']/f'homework/homework-{hw_num}/output'
@@ -42,3 +69,4 @@ class CopyHomework(Task):
                     'actions': [(utils.copy_file, (file, real_dest))],
                     'uptodate': [utils.config_changed(kw, self.name)],
                 }, filters)
+
