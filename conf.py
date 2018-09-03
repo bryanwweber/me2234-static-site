b9ccd7b6ded13287ed930619be3658bc7152d89b
Bryan W. Weber <bryan.w.weber@gmail.com>
2018-09-03 19:42:05 -0400

    Specify that pages should not be cached by the browser

--- conf.py
+++ conf.py
@@ -1190,7 +1190,7 @@ SHOW_SOURCELINK = False
 # Extra things you want in the pages HEAD tag. This will be added right
 # before </head>
 # (translatable)
-# EXTRA_HEAD_DATA = ""
+EXTRA_HEAD_DATA = '<meta http-equiv="Cache-control" content="no-store">'
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
